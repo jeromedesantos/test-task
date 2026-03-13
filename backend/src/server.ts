2612aae624f633e203c03075e6fe9aaa2d2ad659
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2026-03-13 23:01:36 +0700

    chore: move to render

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import http from "http";
+import app from "./app";
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server berjalan di port ${PORT}`);
+});
